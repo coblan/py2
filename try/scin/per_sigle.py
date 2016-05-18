4fd6733b3b53ec326fb7cde4f9e6ac322071e54e
coblan <coblan@163.com>
2016-05-19 01:30:54 +0800

    auto commit

--- try/scin/per_sigle.py
+++ try/scin/per_sigle.py
@@ -1,21 +1,11 @@
 import numpy as np
 
-x=[[-1,1,-2,0],
+x=[[-1,1,-2,0],     # 样本输入
    [-1,0,1.5,-0.5],
-   [-1,-1,1,0.5],
-   [-1,0,1.5,-0.5],
-   [-1,0,1.5,-0.5]]
+   [-1,-1,1,0.5]]
 
-d=np.matrix([[-1,-1,1,-1,-1],
-             [0,1,1,1,1],
-             [0,-1,0,-1,-1]])
-
-for i in range(10):
-    x.append([-1,0,1.5,-0.5])
-    d = np.column_stack((d,[-1,1,-1]))
-
-w=np.matrix([0.5,1,-1,0])
-w=np.row_stack((w,w,w))
+d=np.matrix([[-1,-1,1]])  # 样本输出
+w=np.matrix([0.5,1,-1,0])  # 初始w
 
 def run():
     global w
@@ -31,16 +21,23 @@ def out(w,x):
 
 @np.vectorize
 def transfer(x):
+    # 模拟sign函数，向量化是考虑到输入节点是多个的情况，这时x是一个向量
     if x>=0:
         return 1
     else:
         return -1
 
 
-def learn(w,d,rt,x):
-    print(d-rt)
-    return w+ 0.1*(d-rt)*x
+def learn(w,d,o,x):
+    # 这里就是简单的LMS学习算法
+    print(d-o)
+    return w+ 0.1*(d-o)*x
 
+if __name__=='__main__':
+    run()
+    print(w)
 
-run()
-print(w*np.matrix(x[2]).T)
+    w=np.matrix([0.2,0.5,-1,0])  #重新置w的值，再计算
+    run()
+    print(w)
+    
