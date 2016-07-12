60df8f251f9ab17a839e46832f90f4f7c9ff2273
heyulin <heyulin@smalltreemedia.com>
2016-07-12 08:40:01 +0800

    hhj

--- try/gitp.py
+++ try/gitp.py
@@ -1,17 +1,3 @@
-from pexpect.popen_spawn import PopenSpawn
-from pexpect import EOF
-import sys
-try:
-    child = PopenSpawn(u'cmd',cwd=r'D:\coblan\web\first')
-    child.logfile = sys.stdout
-    child.sendline('git push')
-    child.expect(r"Username for 'https://git.oschina.net':")
-    child.send('coblan@163.com')
-    while True:
-        i =child.expect([r'Username for .*\.net','Password for'])
-        if i==0:
-            child.sendline('coblan@163.com')
-        elif i==1:
-            child.sendline('he7125158')
-except EOF :
-    pass
+import requests
+rt = requests.get('http://stash.stm.com/plugins/servlet/archive/projects/CMP/repos/apppagegenerator?at=refs%2Fheads%2Fmaster')
+print(rt.content)
