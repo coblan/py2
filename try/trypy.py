047f299b0bc6cd222dde29d9adce91af4b70c95a
heyulin <heyulin@hotmail.com>
2016-12-14 00:35:48 +0800

    修改sync函数

--- try/trypy.py
+++ try/trypy.py
@@ -1,18 +1,36 @@
 # encoding:utf-8
 
-from subprocess import Popen,PIPE
+#from subprocess import Popen,PIPE
 
-#p = Popen(u'dir',stdout=PIPE,shell=True)
+##p = Popen(u'dir',stdout=PIPE,shell=True)
+##stdoutdata, stderrdata = p.communicate()
+##print(stdoutdata.decode('gbk'))
+###for data in stdoutdata:
+    ###print(data)
+###print(p.communicate())
+##print('over')
+#import os
+#print(os.getcwd())
+#p =Popen('fab tt',stdout=PIPE,stdin=PIPE)
+## while p.poll():
 #stdoutdata, stderrdata = p.communicate()
-#print(stdoutdata.decode('gbk'))
-##for data in stdoutdata:
-    ##print(data)
-##print(p.communicate())
-#print('over')
-import os
-print(os.getcwd())
-p =Popen('fab tt',stdout=PIPE,stdin=PIPE)
-# while p.poll():
-stdoutdata, stderrdata = p.communicate()
-print(stdoutdata)
+#print(stdoutdata)
+
+import time
+
+def say(tm=time.time()):
+    print(tm)
+
+def say2(tm= lambda:time.time()):
+    print(tm())
+    
+def test():
+    while True:
+        time.sleep(1)
+        say()
+        say2()
+
+if __name__=='__main__':
+    test()
+        
 
