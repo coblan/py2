375c5bc6e65633b89f53c94b4d02eee1f8be1091
coblan <coblan@163.com>
2017-09-12 18:03:06 +0800

    auto commit

--- try/trypy.py
+++ try/trypy.py
@@ -1,11 +1,5 @@
 # encoding:utf-8
-
-class a(object):
-    def say(self):
-        print('jj')
-
-class b(a):
-    pass
-
-bb=b()
-print hasattr(bb,'say')
+import requests
+rt = requests.get("https://ssl.gstatic.com/accounts/static/_/js/k=gaia.gaiafe_glif.zh_CN.ihEUHp_x7EQ.O/m=P9M9H/am=ggAQAgAAgApUEEQIJqAikGE/rt=j/d=0/rs=ABkqax3TM5r73XoVSSe5GPeSZc5Nf_6LyA")
+# rt = requests.get('https://www.google.com')
+print(rt.content)
