ecb213522089b3f99b712f96236ab41977d2479b
coblan <coblan@163.com>
2017-09-21 15:50:41 +0800

    auto commit

--- try/trypy.py
+++ try/trypy.py
@@ -1,5 +1,23 @@
 # encoding:utf-8
-import requests
-rt = requests.get("https://ssl.gstatic.com/accounts/static/_/js/k=gaia.gaiafe_glif.zh_CN.ihEUHp_x7EQ.O/m=P9M9H/am=ggAQAgAAgApUEEQIJqAikGE/rt=j/d=0/rs=ABkqax3TM5r73XoVSSe5GPeSZc5Nf_6LyA")
-# rt = requests.get('https://www.google.com')
-print(rt.content)
+
+with open(r"C:\Users\heyulin\Desktop\pts.txt") as f:
+    ss = f.read()
+    ls = ss.split('\n')
+    ls=[x.split(':') for x in ls]
+    
+    with open(r"C:\Users\heyulin\Desktop\pts_pswd.txt") as f2:
+        zz = f2.read()
+        pswd=zz.split('\n')
+        pswd=[x.split(':') for x in pswd if x]
+        pswd_dc=dict(pswd)
+    
+    yy=[[x[0],x[1],pswd_dc[x[0]] ] for x in ls if len(
+        x)==2]
+    print(yy)
+    outstr_ls=[]
+    outstr="username,email,password\n"
+    for row in yy:
+        outstr_ls.append(','.join(row))
+    outstr +='\n'.join(outstr_ls)
+    with open(r"C:\Users\heyulin\Desktop\pts_pswd.csv",'wb') as f3:
+        f3.write(outstr)
