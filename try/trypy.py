01926a7c2de7fc592f904bd4df5d29e6730834fe
coblan <coblan@163.com>
2017-09-22 18:45:31 +0800

    auto commit

--- try/trypy.py
+++ try/trypy.py
@@ -1,23 +1,27 @@
 # encoding:utf-8
 
-with open(r"C:\Users\heyulin\Desktop\pts.txt") as f:
-    ss = f.read()
-    ls = ss.split('\n')
-    ls=[x.split(':') for x in ls]
+# with open(r"C:\Users\heyulin\Desktop\pts.txt") as f:
+    # ss = f.read()
+    # ls = ss.split('\n')
+    # ls=[x.split(':') for x in ls]
     
-    with open(r"C:\Users\heyulin\Desktop\pts_pswd.txt") as f2:
-        zz = f2.read()
-        pswd=zz.split('\n')
-        pswd=[x.split(':') for x in pswd if x]
-        pswd_dc=dict(pswd)
+    # with open(r"C:\Users\heyulin\Desktop\pts_pswd.txt") as f2:
+        # zz = f2.read()
+        # pswd=zz.split('\n')
+        # pswd=[x.split(':') for x in pswd if x]
+        # pswd_dc=dict(pswd)
     
-    yy=[[x[0],x[1],pswd_dc[x[0]] ] for x in ls if len(
-        x)==2]
-    print(yy)
-    outstr_ls=[]
-    outstr="username,email,password\n"
-    for row in yy:
-        outstr_ls.append(','.join(row))
-    outstr +='\n'.join(outstr_ls)
-    with open(r"C:\Users\heyulin\Desktop\pts_pswd.csv",'wb') as f3:
-        f3.write(outstr)
+    # yy=[[x[0],x[1],pswd_dc[x[0]] ] for x in ls if len(
+        # x)==2]
+    # print(yy)
+    # outstr_ls=[]
+    # outstr="username,email,password\n"
+    # for row in yy:
+        # outstr_ls.append(','.join(row))
+    # outstr +='\n'.join(outstr_ls)
+    # with open(r"C:\Users\heyulin\Desktop\pts_pswd.csv",'wb') as f3:
+        # f3.write(outstr)
+        
+        
+import requests
+'https://dl.google.com/dl/android/maven2/com/android/tools/build/gradle/3.0.0-beta6/gradle-3.0.0-beta6.pom'
