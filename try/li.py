c4a6df0776d0fa5ddb84ee7aebb90154fb3ec6ca
coblan <coblan@163.com>
2018-01-18 17:33:23 +0800

    auto commit

--- try/li.py
+++ try/li.py
@@ -1,6 +1,11 @@
-with open(r'd:/try/jj/OOG104_Resource.csv','rb') as f:
-    aa= f.read()
-    uuaa=aa.decode('gbk')
-    print(aa.decode('gbk'))
-    with open(r'd:/try/jj/OOG104_Resource_utf_8.csv','wb') as f2:
-        f2.write(uuaa.encode('utf-8'))
+
+class AA(object):
+    tt= ''
+
+class BB(AA):
+    class mm(object):
+        name = 'hello'
+    def __init__(self):
+        print('hello')
+
+yy = BB()
